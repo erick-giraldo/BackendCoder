eca2106a01a32bd42ace53028a380041e30df0c6
Erick Giraldo <egiraldom@outlook.com>
2023-04-09 22:15:30 -0500

    web

--- Websocket/src/utils/index.js
+++ Websocket/src/utils/index.js
@@ -1,5 +1,3 @@
-import bcrypt from  "bcrypt"
-
 export const getEmptyProperties = ( object ) => {
     var emptyProperties = [];
     for (var key in object) {
@@ -9,14 +7,3 @@ export const getEmptyProperties = ( object ) => {
     }
     return emptyProperties;
   }
-
-export const createHash = ( password ) =>{
-  return bcrypt.hashSync( password, bcrypt.genSaltSync(10) )
-}
-
-export const validatePassword = ( password, user ) =>{
-  return bcrypt.compareSync( password, user.password )
-}
-
-
-
