3b16139baf6a7201338122dcbd42a37d6dedaad2
Erick <erick.giraldo@banexcoin.com>
2023-06-05 19:53:54 -0500

    avance 1

--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@ import http from 'http'
 import app from './config/app.js'
 import { init } from './config/socket.js'
 import config from './config/index.js'
-
+import getLogger  from "./utils/logger.js";
 const PORT = config.port
 const ENV = process.env.NODE_ENV || 'local'
 
 
 const server = http.createServer(app)
-
+const logger = getLogger();
 init(server)
 
 server.listen(PORT, () => {
-  console.log(`Server running in http://localhost:${PORT}/ in ${ENV} environment.`)
+  logger.info(`Server running in http://localhost:${PORT}/ in ${ENV} environment.`)
 })
---
+++ src/utils/logger.js
@@ -0,0 +1,48 @@
+import winston, { format } from "winston";
+const customerLogger = {
+  levels: {
+    debug: 0,
+    http: 1,
+    info: 2,
+    warning: 3,
+    error: 4,
+    fatal: 5,
+  },
+};
+
+const developmentLogger = winston.createLogger({
+  levels: customerLogger.levels,
+  format: winston.format.combine(
+    winston.format.timestamp({
+        format: "YYYY-MM-DD HH:mm:ss",
+      }),
+    winston.format.simple()
+  ),
+  transports: [new winston.transports.Console({ level: "debug" })],
+});
+
+const productionLogger = winston.createLogger({
+  levels: customerLogger.levels,
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console({ level: "info" }),
+    new winston.transports.File({
+      filename: "./logs/errors.log",
+      level: "error",
+    }),
+  ],
+});
+
+const getLogger = () => {
+  if (process.env.NODE_ENV === "production") {
+    return productionLogger;
+  }
+  return developmentLogger;
+};
+
+export default getLogger;
